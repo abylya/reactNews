c2bc8b501408b8bbecf106c56a1d8c3dba724a04
abdyla <abdyla0303@yandex.ru>
2024-07-21 20:19:40 +0300

    add bannerList

---
+++ src/components/bannerLIst/BannerList.tsx
@@ -0,0 +1,23 @@
+import NewsBanner from "../newsBanner/NewsBanner";
+import styles from "./styles.module.css";
+import withSceleton from "../../helps/hocs/withSceleton";
+import { INews } from "../interfeces";
+//import {Prop} from "../../hocs/withSceleton";
+interface Props{
+  newsList?:INews[]|null;
+}
+function BannerList({ newsList }:Props) {
+  return (
+    <ul className={styles.list}>
+      {newsList?.map((banner) => {
+        return (
+          <li key={banner.id} className={styles.item}>
+            <NewsBanner news={banner}></NewsBanner>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+const BannerListWithSkeleton = withSceleton<Props>(BannerList, 10, "row");
+export default BannerListWithSkeleton;
